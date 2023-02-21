017abac49d226ed22033d0f727027b23b32b66fa
Anillc <i@anillc.cn>
2023-02-21 23:20:04 +0800

    fix build

--- shell.nix
+++ shell.nix
@@ -1,8 +1,11 @@
-{ pkgs ? import <nixpkgs> {} }:
+# new clouse-compiler doesn't support /** global */ in api.js
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/e19f25b587f15871d26442cfa1abe4418a815d7d.tar.gz") {} }:
 
-pkgs.mkShell {
+pkgs.mkShell.override {
+    stdenv = pkgs.emscriptenStdenv;
+} {
     name = "sql.js";
     nativeBuildInputs = with pkgs; [
-        perlPackages.DigestSHA3 emscripten unzip gnumake
+        perlPackages.DigestSHA3 unzip gnumake
     ];
 }
